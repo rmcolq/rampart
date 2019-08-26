a1df9945e93eb7764acea249bf85e9f1ba2a8c7e
Rachel Colqhoun <rachel.colquhoun@ed.ac.uk>
2019-08-26 12:08:42 +0100

    reduce threshold to 0 for reference panel so there are always 20

--- server/datastore.js
+++ server/datastore.js
@@ -136,7 +136,7 @@ Datastore.prototype.getBarcodesSeen = function() {
  * @param {int} threshold at least 1 sample must have over this perc of reads mapping to include
  * @param {int} maxNum max num of references to return
  */
-const whichReferencesToDisplay = (processedData, threshold=0.5, maxNum=20) => {
+const whichReferencesToDisplay = (processedData, threshold=0, maxNum=20) => {
   const refMatchesAcrossSamples = {};
   const refsAboveThres = {}; /* references above ${threshold} perc in any sample. values = num samples matching this criteria */
   for (const [sampleName, sampleData] of Object.entries(processedData)) {
